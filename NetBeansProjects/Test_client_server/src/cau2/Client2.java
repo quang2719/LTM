f64f0b96b1e96775308767154d638446ac81ea86
quang2719 <111571873+quang2719@users.noreply.github.com>
2024-11-06 02:13:39 +0700

    update cau 3

--- NetBeansProjects/Test_client_server/src/cau2/Client2.java
+++ NetBeansProjects/Test_client_server/src/cau2/Client2.java
@@ -14,15 +14,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Admin
- */
 public class Client2 {
     public static void main(String[] args) throws IOException {
         Socket sc = new Socket("localhost",5555);
