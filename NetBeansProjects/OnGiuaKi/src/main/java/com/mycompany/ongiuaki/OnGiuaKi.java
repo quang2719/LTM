2632d1aa66ea7e9c5e51ec98c29aa7e6c0b2d675
quang2719 <111571873+quang2719@users.noreply.github.com>
2024-11-05 23:52:55 +0700

    update practice assignment

---
+++ NetBeansProjects/OnGiuaKi/src/main/java/com/mycompany/ongiuaki/OnGiuaKi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ongiuaki;
+
+/**
+ *
+ * @author Admin
+ */
+public class OnGiuaKi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeansProjects/Test_client_server/src/test_client_server/Test_client_server.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test_client_server;
+
+/**
+ *
+ * @author Admin
+ */
+public class Test_client_server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
