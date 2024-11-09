5c72c86574e9d8e99cd4565c2a39e472aca0c3df
quang2719 <111571873+quang2719@users.noreply.github.com>
2024-11-09 15:31:15 +0700

    new test file

--- NetBeansProjects/Test_client_server/src/cau3/test.java
+++ NetBeansProjects/Test_client_server/src/cau3/test.java
@@ -4,10 +4,32 @@
  */
 package cau3;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  *
  * @author Admin
  */
 public class test {
-    
+    public static void main(String[] args) {
+        String s= "asdfasdfsadf12";
+        Map<Character,Integer> mp = new HashMap<>();
+        for (int i = 0;i< s.length();i++){
+            if (!mp.containsKey(s.charAt(i))){
+                mp.put(s.charAt(i), 1);
+            }else{
+                int count = mp.get(s.charAt(i));
+                mp.remove(s.charAt(i));
+                mp.put(s.charAt(i), count+1);
+            }
+        }
+        for(Character c :mp.keySet()){
+            System.out.println(c + "-" + (mp.get(c) +"0"));
+        }
+//        System.out.println(mp.toString());
+        
+    }
 }
