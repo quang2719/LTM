c18644bb41c1169c28ea7924a1ba95e169e3ae96
quang2719 <111571873+quang2719@users.noreply.github.com>
2024-11-09 15:31:27 +0700

    fix client file

--- NetBeansProjects/Test_client_server/src/cau3/Client.java
+++ NetBeansProjects/Test_client_server/src/cau3/Client.java
@@ -5,9 +5,13 @@
 package cau3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  *
@@ -16,6 +20,39 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket sc = new Socket("localhost",5555);
-        BufferedReader = new BufferedReader(new InputStreamReader)
+        BufferedReader br = new BufferedReader(new InputStreamReader(sc.getInputStream()));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(sc.getOutputStream()));
+        
+        String code = "MSV;qcode";
+        bw.write(code);
+        bw.newLine();
+        bw.flush();
+        System.out.println("Sending code to server: "+code);
+        
+        String input = br.readLine();
+        String filter_input = "";
+        for (String i : input.split(" ")){
+            filter_input += i;
+        }
+        System.out.println("Received input: "+input);
+        
+        Map<Character,Integer> mp = new Hashtable<>();
+        for (int i = 0;i< filter_input.length();i++){
+            mp.put(filter_input.charAt(i), mp.getOrDefault(filter_input.charAt(i), 0) +1);
+        }
+        String res = "";
+        for (Character c : mp.keySet()){
+            int val = mp.get(c);
+            if (val >1){
+                res += c+":"+val+",";
+            }
+        }
+        res = res.substring(0, res.length()-1);
+        System.out.println("Sendidng to server: "+res);
+        bw.write(res);
+        bw.flush();
+        bw.close();
+        br.close();
+        sc.close();
     }
 }
