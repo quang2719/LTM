a63f13306ab5367953ef5d252329d77be36e4180
quang2719 <111571873+quang2719@users.noreply.github.com>
2024-11-09 14:46:06 +0700

    update assignment file

--- NetBeansProjects/Test_client_server/src/cau3/Client.java
+++ NetBeansProjects/Test_client_server/src/cau3/Client.java
@@ -4,10 +4,18 @@
  */
 package cau3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  *
  * @author Admin
  */
 public class Client {
-    
+    public static void main(String[] args) throws IOException {
+        Socket sc = new Socket("localhost",5555);
+        BufferedReader = new BufferedReader(new InputStreamReader)
+    }
 }
